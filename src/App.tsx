9ca3d37d6a1566b10c39a6c2001460f17957d073
Thays <thays.santos@omeletecompany.com>
2021-05-18 14:01:55 -0300

    Ignite ReactJS - Cap II Módulo 1- Criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import styled from 'styled-components' 
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e7
-`;
-
+import {GlobalStyle} from './styles/global';
 
 export function App() {
   return (
     <div className="App">
-      <Title>
+      <GlobalStyle></GlobalStyle>
+      <h1>
         Coé
-      </Title>
+      </h1>
     </div>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+:root{
+    --red: #E52E4D;
+    --purple: #5429CC;
+    --light-purple: #6933FF;
+    --green: #33CC95
+
+    --background: #F0F2F5
+    --title-text: #363F5F;
+    --text-body: #969CB3;
+    --shape: #FFFFFF;
+}
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html{
+    @media(max-width: 1080px){
+        font-size: 93.75%; // 15px
+    }
+    
+    @media(max-width: 720px){
+        font-size: 87.5%; // 14px
+    }
+}
+
+body{
+    background-color: var(--background);
+    -webkit-font-smoothing: antialiased;
+}
+
+button {
+    cursor: pointer;
+}
+
+[disabled]{
+    opacity: 0.6;
+    cursor: not-allowed;
+}
+`
