ac4a7f5c53f7b9a618d3d782df69864e0c4958be
Thays <thays.santos@omeletecompany.com>
2021-05-18 14:06:48 -0300

    Ignite ReactJS - Cap II Módulo 1- Adicionando fontes do projeto

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -35,8 +35,17 @@ body{
     -webkit-font-smoothing: antialiased;
 }
 
+body, input, textarea, button{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+}
+
 button {
     cursor: pointer;
+}
+
+h1,h2,h3,h4,h5,h6, strong{
+    font-weight: 600;
 }
 
 [disabled]{
